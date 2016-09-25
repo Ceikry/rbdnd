fa044392921a3fa1c25eb9029731a65d19d9aea2
Rick Chamblee <rchamblee@sdf.org>
2016-09-25 09:10:33 -0500

    Added V0.01

---
+++ README.md
@@ -0,0 +1,8 @@
+rbdnd is my first attempt at making any kind of 2d game. It is entirely text-based and does not use curses.
+<br/>
+For the 2d plane, I have a vector, y, and for each element in this vector I have another vector that represents the x plane on the given y plane. 
+<br/>
+These vectors can then be populated with chars to add elements to the map.
+<br/>
+Current time spent on project: approximately 15-20 minutes.....
+
---
+++ main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <string>
+
+using namespace std;
+
+vector<vector<char> >y;
+char pChar = '@';
+
+void squareMap(int size){
+    y.reserve(size);
+    int height = size;
+    int width = height;
+    for(int i=0; i < height; i++){
+        vector<char>x;
+        width = height * 2;
+        x.reserve(width);
+        for(int k = 0; k < x.capacity(); k++){
+            x.push_back('.');
+        }
+        y.push_back(x);
+    }
+}
+
+void printMap(){
+    for(int i=0; i < y.size(); i++){
+        for(int k=0; k < y[i].size(); k++){
+            cout << y[i][k];
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    string u_input;
+    squareMap(5);
+    int iYp = y.size() / 2;
+    int iXp = y[iYp].size() / 2;
+    while(true){
+        y[iYp][iXp] = pChar;
+        printMap();
+        y[iYp][iXp] = '.';
+        cout << endl << endl << endl;
+        getline(cin,u_input);
+        if(u_input == "mv up"){
+            iYp = iYp - 1;
+        }else if(u_input == "mv down"){
+            iYp = iYp + 1;
+        }else if(u_input == "mv left"){
+            iXp = iXp - 1;
+        }else if(u_input == "mv right"){
+            iXp = iXp + 1;
+        }
+    }
+    return 0;
+}
